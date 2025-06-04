d6ad99447e9ea78551aea69521501cf66b1cecd6
XxemicoxX <emiliojc2004@gmail.com>
2025-06-03 21:47:22 -0500

    03/06/25-21:44

--- src/main/java/com/example/integrador/services/RolService.java
+++ src/main/java/com/example/integrador/services/RolService.java
@@ -13,16 +13,20 @@ import lombok.RequiredArgsConstructor;
 public class RolService {
 
     private final RolRepository repository;
+
     public List<Rol> rolSel(){
         return repository.findAll();
     }
+
     public Rol rolSelectOne(Integer id) {
         return repository.findById(id).orElse(null);
     }
+
     // INSERT into Rol && UPDATE dinosaurio SET
     public Rol rolInsertUpdate(Rol rol){
         return repository.save(rol);
     }
+    
      public void rolDelete(Integer id) {
         repository.deleteById(id);
     }
