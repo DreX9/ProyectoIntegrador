c12ee86876189fa384fff44e72d542cc871fbdfe
XxemicoxX <emiliojc2004@gmail.com>
2025-06-09 09:51:13 -0500

    09/06/25-09:51

--- src/main/java/com/example/integrador/controllers/NavigatorController.java
+++ src/main/java/com/example/integrador/controllers/NavigatorController.java
@@ -1,6 +1,5 @@
 package com.example.integrador.controllers;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,33 +17,3 @@ public class NavigatorController {
         return "index";
     }
 }
-=======
- import org.springframework.stereotype.Controller;
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.RequestMapping;
-
- @Controller
- @RequestMapping
- public class NavigatorController {
-    
-     @GetMapping("/ventas")
-     public String ventas(){
-         return "ventas";   
-     }
-
-     @GetMapping("/compras")
-     public String compras(){
-         return "compras";   
-     }
-
-     @GetMapping("/inventarios")
-     public String inventarios(){
-         return "inventarios";   
-     }
-
-     @GetMapping("/reportes")
-     public String reportes(){
-         return "reportes";   
-     }
- }
->>>>>>> 3a9205c0100de1cbb35e39e2a8a075ac97623551
